a3603a943e7baca459e5808f8285089bdc7652a2
minh <minh@LAPTOP_MARCH>
2023-09-27 09:12:17 +0700

    Add project files.

---
+++ Controllers/KhoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFTEST1.Controllers
+{
+    public class KhoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Entity/MasterContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFTEST1.Models.Entity;
+
+public partial class MasterContext : DbContext
+{
+    public MasterContext()
+    {
+    }
+
+    public MasterContext(DbContextOptions<MasterContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=Def");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
